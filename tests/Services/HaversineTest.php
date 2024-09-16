9a3d59774b5bfb2031ea0b75bae4aae64e4399df
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-09-16 09:54:44 +0200

    Haversine service

---
+++ tests/Services/HaversineTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Services\HaversineService;
+
+it('calculates the correct distance between Turin and Milan', function () {
+    $turin = [45.0703, 7.6869];
+    $milan = [45.4642, 9.1900];
+
+    $calculator = new HaversineService();
+
+    $distance = $calculator->execute($turin, $milan);
+
+    expect($distance)->toBeBetween(125.50, 125.52   );
+});
