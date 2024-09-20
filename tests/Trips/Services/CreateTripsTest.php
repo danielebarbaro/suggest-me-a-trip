46a95998a19f02a3521804fa453efa319a753b39
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-09-20 23:25:01 +0200

    create trips tests with errors

--- tests/Trips/Services/CreateTripsTest.php
+++ tests/Trips/Services/CreateTripsTest.php
@@ -4,59 +4,53 @@ use App\Stations\Station;
 use App\Trips\Services\CreateTripsService;
 use App\Trips\Trip;
 use App\Utils\GeoCoderService;
+use Geocoder\Provider\Provider;
 use Library\RoadSurfer\DTO\CityDTO;
 use Library\RoadSurfer\DTO\StationDTO;
 use Library\RoadSurfer\RoadSurfer;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 beforeEach(function () {
+    $_ENV['GEO_CACHE_TTL'] = '3600';
+
     $this->roadSurfer = Mockery::mock(RoadSurfer::class);
     $this->geoCoderService = Mockery::mock(GeoCoderService::class);
-    $this->createTripsService = new CreateTripsService($this->roadSurfer);
+    $this->providerMock = Mockery::mock(Provider::class);
+    $this->cacheAdapter = new ArrayAdapter();
+    $this->createTripsService = new CreateTripsService(
+        $this->roadSurfer,
+        $this->providerMock,
+        $this->cacheAdapter
+    );
 
-    $this->station1 =
-        Mockery::mock(
-            Station::class,
-            [
-                'id' => '1',
-                'name' => 'Turin',
-                'fullName' => 'Turin, Italy',
-                'country' => 'Italy',
-                'coordinates' => [45.0703, 7.6869],
-            ]
-        );
-    $this->station2 =
-        Mockery::mock(
-            Station::class,
-            [
-                'id' => '2',
-                'name' => 'Frankfurt',
-                'fullName' => 'Frankfurt, Germany',
-                'country' => 'Germany',
-                'coordinates' => [50.1109, 8.6821],
-            ]
-        );
-    $this->station3 =
-        Mockery::mock(
-            Station::class,
-            [
-                'id' => '3',
-                'name' => 'Bordeaux',
-                'fullName' => 'Bordeaux, France',
-                'country' => 'France',
-                'coordinates' => [44.8416106, -0.5810938],
-            ]
-        );
-    $this->station4 =
-        Mockery::mock(
-            Station::class,
-            [
-                'id' => '4',
-                'name' => 'Milan',
-                'fullName' => 'Milan, Italy',
-                'country' => 'Italy',
-                'coordinates' => [45.4642700, 9.1895100],
-            ]
-        );
+    $this->station1 = new Station(
+        1,
+        'Turin',
+        'Turin, Italy',
+        'Italy',
+        [45.0703, 7.6869],
+    );
+    $this->station2 = new Station(
+        2,
+        'Frankfurt',
+        'Frankfurt, Germany',
+        'Germany',
+        [50.1109, 8.6821],
+    );
+    $this->station3 = new Station(
+        3,
+        'Bordeaux',
+        'Bordeaux, France',
+        'France',
+        [44.8416106, -0.5810938],
+    );
+    $this->station4 = new Station(
+        4,
+        'Milan',
+        'Milan, Italy',
+        'Italy',
+        [45.4642700, 9.1895100],
+    );
 });
 
 afterEach(function () {
@@ -85,19 +79,7 @@ it('returns empty array when there are no rally stations', function () {
 });
 
 it('removes duplicate countries and converts them to lowercase', function () {
-    $pickupStation = Mockery::mock(StationDTO::class, [
-        'city' => Mockery::mock(CityDTO::class, ['countryName' => 'Italy']),
-    ]);
-
-    $dropoffStation1 = Mockery::mock(StationDTO::class, [
-        'city' => Mockery::mock(CityDTO::class, ['countryName' => 'Germany']),
-    ]);
-
-    $dropoffStation2 = Mockery::mock(StationDTO::class, [
-        'city' => Mockery::mock(CityDTO::class, ['countryName' => 'France']),
-    ]);
-
-    $countries = $this->createTripsService->getUniqueCountries($pickupStation, [$dropoffStation1, $dropoffStation2]);
+    $countries = $this->createTripsService->getUniqueCountries($this->station1, [$this->station4, $this->station3]);
 
     expect(array_values($countries))->toBe(['italy', 'france']);
 });
